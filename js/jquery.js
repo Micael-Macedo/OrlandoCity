2b1c213b5b720d29c162b22d17234ebc69f2b8b5
Micael Macedo <micaelm2009@hotmail.com>
2022-12-30 17:27:38 -0300

    feat: add responsive content

---
+++ js/jquery.js
@@ -0,0 +1,46 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:4,
+            nav:true,
+            loop:false
+        }
+    }
+})
+
+$(function(){
+    $(".thumbnails .item").on("click", function(){
+        $("video").attr({
+            "src":"video/"+$(this).data('video')+".mp4",
+            "poster":"img/"+$(this).data('video')+".jpg"
+        });
+    });
+
+    $("#volumeVideo").on("mousemove",function(){
+        $("video")[0].volume = $(this).val();
+    });
+
+    $("#btn-play-pause").on("click", function(){
+        $(this).toggleClass("btn-sucess btn-danger")
+        var video = $("video")[0];
+
+        if($(this).hasClass("btn-sucess")){
+            $(this).text("STOP");
+            video.play();
+        }else{
+            $(this).text("PLAY")
+            video.pause();
+        }
+    });
+    const player = new Plyr('#player');
+});
